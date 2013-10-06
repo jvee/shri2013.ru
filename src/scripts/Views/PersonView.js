bb7df1d47d8dc922073b4a7d662013276b1c965a
Ivan Zasimoff <jvee99@gmail.com>
2013-10-06 21:11:27 +0400

    Базовая имплементация PersonView

---
+++ src/scripts/Views/PersonView.js
@@ -0,0 +1,14 @@
+var PersonView = Backbone.View.extend({
+
+	tagName: 'article',
+
+	className: 'b-person',
+
+	template: tmpl['PersonTemplate'],
+
+	render: function () {
+		this.el.innerHTML = this.template(this.model.toJSON());
+
+		return this;
+	}
+});
---
+++ test/PersonView/spec.js
@@ -0,0 +1,42 @@
+suite('PersonView', function () {
+
+	setup(function () {
+		model = new Backbone.Model(fixtures.person1);
+		person = new PersonView({ model: model });
+	});
+
+	suite('Common', function () {
+		test('should exist', function () {
+			assert.isNotNull(PersonView, 'as global var');
+			assert.typeOf(PersonView, 'function', 'as constructor');
+		});
+	});
+
+	suite('#initialize()', function () {
+
+		test('should have right defaults', function () {
+			assert.equal(person.el.tagName.toLowerCase(), 'article', 'tagName');
+			assert.equal(person.el.className, 'b-person', 'className');
+			assert.typeOf(person.template, 'function', 'template must be precompiled');
+		});
+
+	});
+
+	suite('#render()', function () {
+
+		setup(function () {
+			testTemplate = tmpl.PersonTemplate(fixtures.person1);
+			person.render();
+		});
+
+		test('should render', function () {
+			assert.equal(person.el.innerHTML, testTemplate, 'right HTML');
+		});
+
+		test('should return', function () {
+			assert.equal(person.render(), person, 'PersonView object');
+		});
+
+	});
+
+});
