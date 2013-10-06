8b20e8adb04b2d7384957125c2065c1864a5881d
Ivan Zasimoff <jvee99@gmail.com>
2013-10-07 00:56:04 +0400

    Базовая имплементация PersonListView

---
+++ src/scripts/Views/PersonListView.js
@@ -0,0 +1,21 @@
+var PersonListView = Backbone.View.extend({
+
+	tagName: 'section',
+
+	className: 'b-person-list',
+
+	render: function () {
+		this.collection.each(this.addPerson, this);
+
+		return this;
+	},
+
+	addPerson: function (model) {
+		var personView = new PersonView({ model: model});
+
+		this.el.innerHTML += personView.render().el.innerHTML;
+
+		return this;
+	}
+
+});
