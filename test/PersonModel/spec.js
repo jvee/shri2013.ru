5f6dcb3f8fa598d075ed6860c5e0f5396bd2bff2
Ivan Zasimoff <jvee99@gmail.com>
2013-10-06 21:08:50 +0400

    Обновлены тест-файлы PersonModel

--- test/PersonModel/spec.js
+++ test/PersonModel/spec.js
@@ -21,25 +21,15 @@ suite('PersonModel', function () {
 		});
 
 		test('should accept right data', function () {
-			var fixture = {
-				name: 'Ivan',
-				photo: 'http://url.ru/me.jpg',
-				about: 'Some words',
-				profiles: {
-					github: 'https://github.com/ivan',
-					vkontakte: 'http://vk.com/ivan',
-					facebook: 'http://facebook.com/ivan'
-				}
-			};
-
-			var person = new PersonModel(fixture);
+			var fixture = fixtures.person1,
+				person = new PersonModel(fixture);
 
 			assert.equal(person.get('name'), fixture.name, 'name');
 			assert.equal(person.get('photo'), fixture.photo, 'photo');
 			assert.equal(person.get('about'), fixture.about, 'about');
 			assert.deepEqual(person.get('profiles'), fixture.profiles, 'profiles');
 
-		});	
+		});
 
 	});
 
