c15db90766b0190028ba88ef5fb850c4b26137a4
Ivan Zasimoff <jvee99@gmail.com>
2013-10-13 18:26:36 +0400

    Имплементация #parsePages() и обновление #initialize() AppView

--- src/scripts/Views/AppView.js
+++ src/scripts/Views/AppView.js
@@ -1,12 +1,45 @@
-App.AppView = Backbone.View.extend({
+;(function (window, $, App) {
 
-	el: document.body,
+	'use strict';
 
-	template: tmpl['AppTemplate'],
+	App.AppView = Backbone.View.extend({
 
-	render: function () {
-		this.el.innerHTML = this.template();
+		el: document.body,
 
-		return this;
-	}
-});
+		template: tmpl['AppTemplate'],
+
+		initialize: function (options) {
+			this.menu = new App.MenuCollection(options.data.menu || []);
+			this.menuView = new App.MenuView({ collection: this.menu });
+
+			this.pages = [];
+			this.parsePages(options.data.pages);
+
+			this.render();
+
+			this.$headerEl = this.$('.b-header');
+			this.headerEl = this.$headerEl[0];
+			this.headerEl.appendChild(this.menuView.render().el);
+
+			this.$pageEl = this.$('.b-page');
+			this.pageEl = this.$pageEl[0];
+		},
+
+		parsePages: function (pages) {
+			var x, page;
+
+			for (x = 0; x < pages.length; x++) {
+				page = {};
+				page[pages[x].url] = new App[pages[x].type](pages[x].data);
+				this.pages.push(page);
+			}
+		},
+
+		render: function () {
+			this.el.innerHTML = this.template();
+
+			return this;
+		}
+	});
+
+})(this, jQuery, App = window.App || {}, undefined);
--- test/AppView.js
+++ test/AppView.js
@@ -1,9 +1,18 @@
 suite('AppView', function () {
 
 	setup(function () {
-		app = new App.AppView({
-			el: document.createElement('body')
+		this.renderSpy = sinon.spy(App.AppView.prototype, 'render');
+		this.parsePagesSpy = sinon.spy(App.AppView.prototype, 'parsePages');
+
+		this.app = new App.AppView({
+			el: document.createElement('body'),
+			data: App.data
 		});
+	});
+
+	teardown(function () {
+		this.renderSpy.restore();
+		this.parsePagesSpy.restore();
 	});
 
 	suite('Common', function () {
@@ -16,8 +25,42 @@ suite('AppView', function () {
 	suite('#initialize()', function () {
 
 		test('should have right defaults', function () {
-			assert.equal(app.el.tagName.toLowerCase(), 'body', 'tagName');
-			assert.typeOf(app.template, 'function', 'template must be precompiled');
+			assert.equal(this.app.el.tagName.toLowerCase(), 'body', 'tagName');
+			assert.typeOf(this.app.template, 'function', 'template must be precompiled');
+		});
+
+		test('should create attributes', function () {
+			assert.ok(this.app.menu, '#menu');
+			assert.ok(this.app.menu instanceof App.MenuCollection, '#menu as App.MenuCollection instance');
+			assert.ok(this.app.menuView, '#menuView');
+			assert.ok(this.app.menuView instanceof App.MenuView, '#menuView as App.MenuView instance');
+			assert.ok(this.app.$pageEl, '#$pageEl');
+			assert.ok(this.app.pageEl, '#pageEl');
+			assert.ok(this.app.$headerEl, '#$headerEl');
+			assert.ok(this.app.headerEl, '#headerEl');
+			assert.ok(this.app.pages, '#pages');
+			assert.typeOf(this.app.pages, 'array');
+		});
+
+		test('should call methods', function () {
+			assert.ok(this.renderSpy.calledOnce, '#render()');
+			assert.ok(this.parsePagesSpy.calledOnce, '#parsePages()');
+		});
+
+		test('should add content', function () {
+			assert.notEqual(this.app.headerEl.innerHTML, 'to #headerEl');
+		});
+
+	});
+
+	suite('#parsePages', function () {
+
+		setup(function () {
+			console.log(this.app.pages);
+		});
+
+		test('should update #pages.length', function () {
+			assert.notEqual(this.app.pages.length, 0);
 		});
 
 	});
@@ -25,16 +68,15 @@ suite('AppView', function () {
 	suite('#render()', function () {
 
 		setup(function () {
-			testTemplate = tmpl.AppTemplate();
-			app.render();
+			this.app.render();
 		});
 
 		test('should render', function () {
-			assert.equal(app.el.innerHTML, testTemplate, 'right HTML');
+			assert.equal(this.app.el.innerHTML, tmpl.AppTemplate(), 'right HTML');
 		});
 
 		test('should return', function () {
-			assert.equal(app.render(), app, 'AppView object');
+			assert.equal(this.app.render(), this.app, 'AppView object');
 		});
 
 	});
