32fb1563d8f234bddd070e1ba494216074fa3b4a
Ivan Zasimoff <jvee99@gmail.com>
2013-10-14 04:11:32 +0400

    Описаны начальные данные и обновлены фикстуры

--- test/fixtures/templates.js
+++ test/fixtures/templates.js
@@ -6,12 +6,12 @@ var tmpl = {};
 var out='<div class="b-header"></div><div class="b-page"></div>';return out;
 };
   tmpl['LectureTemplate']=function anonymous(it) {
-var out='<div class="b-lecture__title">'+(it.title)+'</div><div class="b-lecture__description">'+(it.description)+'</div>';if((it.videoUrl || it.presentationUrl)){out+='<ul class="b-lecture__links-list">';if(it.presentationUrl){out+='<li class="b-lecture__link b-lecture__link_type_presentation"><a href="'+(it.presentationUrl)+'">'+(it.presentationUrl)+'</a></li>';}if(it.videoUrl){out+='<li class="b-lecture__link b-lecture__link_type_video"><a href="'+(it.videoUrl)+'">'+(it.videoUrl)+'</a></li>';}out+='</ul>';}return out;
+var out='<div class="b-lecture__title b-block__title"><h2 class="b-lecture__header b-block__header">'+(it.title)+'</h2>';if((it.videoUrl || it.presentationUrl)){out+='<ul class="b-lecture__links-list b-block__link-list">';if(it.presentationUrl){out+='<li class="b-lecture__link b-lecture__link_type_presentation b-block__link-item"><a href="'+(it.presentationUrl)+'">Презентация</a></li>';}if(it.videoUrl){out+='<li class="b-lecture__link b-lecture__link_type_video b-block__link-item"><a href="'+(it.videoUrl)+'">Видео</a></li>';}out+='</ul>';}if(it.lecturer){out+='<h3 class="b-lecture__lecturer b-block__subheader">'+(it.lecturer)+'</h3>';}out+='</div><div class="b-block__content b-text">';if(it.description){out+=(it.description);}out+='</div>';return out;
 };
   tmpl['MenuItemTemplate']=function anonymous(it) {
 var out='<a class="b-menu-item__link';if(it.active){out+=' is-active';}out+='" href="'+(it.url)+'">'+(it.title)+'</a>';return out;
 };
   tmpl['PersonTemplate']=function anonymous(it) {
-var out='<div class="b-person__name">'+(it.name)+'</div>';if(it.photo){out+='<img class="b-person__photo" src="'+(it.photo)+'" alt="'+(it.name)+'">';}out+='<div class="b-person__about">'+( it.about)+'</div>';if(it.profiles){out+='<ul class="b-person__profile-list">';var arr1=it.profiles;if(arr1){var profile,index=-1,l1=arr1.length-1;while(index<l1){profile=arr1[index+=1];out+='<li class="b-person__profile-item"><a href="'+(profile.link)+'">'+(profile.name)+'</a></li>';} } out+='</ul>';}return out;
+var out='<div class="b-person__title b-block__title"><h2 class="b-block__header">'+(it.name)+'</h2>';if(it.profiles){out+='<ul class="b-person__link-list b-block__link-list">';var arr1=it.profiles;if(arr1){var profile,index=-1,l1=arr1.length-1;while(index<l1){profile=arr1[index+=1];out+='<li class="b-person__link-item b-person__link-item_type_'+(profile.name.toLowerCase())+' b-block__link-item"><a href="'+(profile.link)+'">'+(profile.name)+'</a></li>';} } out+='</ul>';}out+='</div><div class="b-block__content">';if(it.photo){out+='<img class="b-person__photo" src="'+(it.photo)+'" alt="'+(it.name)+'">';}out+='<div class="b-person__about b-text">'+( it.about)+'</div></div>';return out;
 };
 return tmpl;})()
