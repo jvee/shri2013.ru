482a6e993df6fd5cc70c5faa3398fb5feb26b394
Ivan Zasimov <ivan.zasimov@actis.ru>
2013-10-16 16:49:05 +0400

    Настроено кэширование data.json в LocalStorage

--- src/scripts/cache.js
+++ src/scripts/cache.js
@@ -6,15 +6,22 @@
 
 	if (!localVersion || localVersion !== App.version) {
 		
-		localStorage.clear();
-
 		$.get(App.stylesLink).then(function (css, data, xhr) {
 			style.innerHTML = css;
 			localStorage['css'] = css;
 		});
 
+		$.ajax(App.dataLink, {
+			async: false,
+			complete: function (xhr, status) {
+				App.data = $.parseJSON(xhr.responseText);
+				localStorage['app.data'] = xhr.responseText;
+			}
+		});
+
 	} else {
 		style.innerHTML = localStorage['css'];
+		App.data = $.parseJSON(localStorage['app.data']);
 	}
 
 	head.appendChild(style);
